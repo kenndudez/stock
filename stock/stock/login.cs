cc5006532c30e6c83a69ba336da4f3fe897c786a
kenndudez <42351751+kenndudez@users.noreply.github.com>
2018-12-15 18:15:04 +0100

    Added stock project

---
+++ stock/stock/login.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace stock
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+        private void login_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void UserName(object sender, EventArgs e)
+        {
+
+        }
+        private void Password(object sender, EventArgs e)
+        {
+
+        }
+        // clear Button 
+        private void button1_click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Clear();
+            textBox1.Focus();
+        }  // Login Button 
+        private void button2_click(object sender, EventArgs e)
+        {
+            //TO-DO: Check user name and password
+            SqlConnection con = new SqlConnection("Data Source=ELITE2560P;Initial Catalog=stock;Integrated Security=True");
+            SqlDataAdapter sda = new SqlDataAdapter(@"SELECT *
+  FROM [dbo].[Login] Where UserName='" + textBox1.Text + "' and Password='" + textBox2.Text + "'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count == 1)
+            {
+                this.Hide();
+                StockMain main = new StockMain();
+                main.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username and Password...!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                button1_click(sender, e);
+            }
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
